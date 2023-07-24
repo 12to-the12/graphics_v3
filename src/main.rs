04debdb4cc93aff0c9963b204b09487d5bd2ee39
Logan Hillyer <shivastranded@proton.me>
2023-07-24 21:28:31 +0700

    basic pixel putting in place with saving to image

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,16 @@ use image::{ImageBuffer, Rgb, RgbImage};
 extern crate stopwatch;
 use stopwatch::Stopwatch;
 
-// const WIDTH: u32 = 3840;
-// const HEIGHT: u32 = 2160;
+struct Point {
+    x: i32,
+    y: i32,
+}
 
-// const WIDTH: u32 = 1920;
-// const HEIGHT: u32 = 1080;
+const WIDTH: u32 = 255;
+const HEIGHT: u32 = 255;
 
-const WIDTH: u32 = 960;
-const HEIGHT: u32 = 540;
+const COLOR: u8 = 200;
 
-// const WIDTH: u32 = 100;
-// const HEIGHT: u32 = 100;
-
-const COLOR: u8 = 0;
-
-// fn main() {
-//     let mut count = 0;
-//     let sw = Stopwatch::start_new();
-//     loop {
-//         count += 1;
-//         if sw.elapsed_ms() > 1000 {
-//             break;
-//         }
-//     }
-//     println!("{}", count);
-// }
 fn main() {
     // ImageBuffer is static, while DynamicImage is dynamic
     // new variable canvas, mutable, type RgbImage, equal to a new ImageBuffer of WIDTH and HEIGHT
@@ -35,38 +20,66 @@ fn main() {
 
     let sw = Stopwatch::start_new();
     // do something that takes some time
-    let frames = 100;
-    for _x in 0..frames {
-        draw(&mut canvas);
-    }
-    let ms = sw.elapsed_ms() as f64;
-    let seconds = ms / 1000.0;
-    let fps = frames as f64 / seconds;
-    let pixels = (WIDTH as i64) * (HEIGHT as i64) * frames;
-    let mega = 1.0 / 1_000_000.0;
-    println!("got {}ms per frame", ms / frames as f64);
-    println!("got {} frames per second", fps);
-    println!(
-        "got {} megapixels through per second",
-        pixels as f64 / seconds * mega
-    );
-    println!(
-        "or {} megabytes through per second",
-        pixels as f64 / seconds * mega * 24.0
-    );
 
-    // println!("120 fps is {}ms per frame", 1000.0/120.0);
-    println!("60 fps is {}ms per frame", 1000.0 / 60.0);
-    println!("30 fps is {}ms per frame", 1000.0 / 30.0);
-    // println!("15 fps is {}ms per frame", 1000.0/15.0);
-    canvas.save("output.png").unwrap();
+    let point_0 = Point { x: 0, y: 0 };
+
+    // for x in 0..100 {
+    //     // draw(&mut canvas);
+    //     let point_1 = Point { x: 2 as i32, y: 50 };
+    //     draw_line(&mut canvas, &point_0, &point_1);
+    // }
+    draw(&mut canvas);
+    println!("{} ms", sw.elapsed_ms());
+    println!("saving");
+    canvas.save("output.bmp").unwrap();
+    println!("and saved");
+
+    // let mut result = factorial(10);
+    // let mut result = 0;
+    // println!("{}", result);
+    // let 🦀 = 12;
+    let sw = Stopwatch::start_new();
+    // println!("{}", result);
+    println!("{} ms", sw.elapsed_ms());
 }
 
+
+// fn interpolate(i0: u32, d0: u32, i1: u32, d1: u32) {
+//     let a = (d1 - d0) / (i1 - i0);
+//     let mut d = d0;
+//     for _i in i0..i1 {
+//         println!("{}", d);
+//         d += a;
+//     }
+// }
+// fn draw_line(canvas: &mut RgbImage, p0: &Point, p1: &Point) {
+//     let mut p0 = p0;
+//     let mut p1 = p1;
+//     if p0.x > p1.x {
+//         // println!("called");
+//         // let (p1, p0) = (p0, p1);
+
+//         let temp = p0;
+//         p0 = p1;
+//         p1 = temp;
+//     }
+//     canvas.put_pixel(p0, p1, Rgb([255, 225, 255]));
+// }
+// fn box_blur(canvas: &RgbImage, size: u32) -> RgbImage {
+//     let mut blurred_canvas: RgbImage = canvas.clone();
+//     // loop over every pixel, then loop over it's effect
+//     for pixel in canvas.enumerate_pixels(){
+//         blurred_canvas.put_pixel(pixel.0, pixel.1, pixel.2);
+//     }
+//     blurred_canvas
+//
+// }
 fn draw(canvas: &mut RgbImage) {
     for x in 0..WIDTH {
         for y in 0..HEIGHT {
             // println!("{}",(y ;
-            canvas.put_pixel(x, y, Rgb([COLOR, COLOR, COLOR]));
+
+            canvas.put_pixel(x, y, Rgb([x as u8, y as u8, ((x+y)/2) as u8]));
             // canvas.put_pixel(x, y, Rgb([(y as f64/8.0) as u8, 255, (x as f64/8.0) as u8]));
         }
     }
